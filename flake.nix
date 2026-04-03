9bc6a6e9b0a0bdb79ab74cb1e3109da6d50284ed
Syner <ploscarusynxy@gmail.com>
2026-04-03 16:21:03 +0000

    Fixed flake.nix

--- flake.nix
+++ flake.nix
@@ -17,7 +17,8 @@
       perSystem = {pkgs, ...}: {
         packages.default = pkgs.writeShellApplication {
           name = "synfetch";
-          text = ./synfetch;
+          excludeShellChecks = ["SC2034" "SC2155" "SC2012" "SC2086" "SC1091" "SC2005" "SC2126"];
+          text = builtins.readFile ./synfetch;
         };
       };
     };
