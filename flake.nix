d254621456bb0be0bc049914fe346e9a666e2613
Syner <ploscarusynxy@gmail.com>
2026-04-03 21:35:28 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,14 +1,23 @@
-diff --git a/flake.nix b/flake.nix
-index dc6bd97..a33b15a 100644
---- a/flake.nix
-+++ b/flake.nix
-@@ -17,7 +17,8 @@
-       perSystem = {pkgs, ...}: {
-         packages.default = pkgs.writeShellApplication {
-           name = "synfetch";
--          text = ./synfetch;
-+          # excludeShellChecks = ["SC2034" "SC2155" "SC2012" "SC2086" "SC1091" "SC2005" "SC2126"];
-+          text = builtins.readFile ./synfetch;
-         };
-       };
-     };
+{
+  description = "synfetch - Synthwave system fetch tool";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      perSystem = { pkgs, ... }: {
+        packages.default = pkgs.writeShellApplication {
+          name = "synfetch";
+
+          text = builtins.readFile ./synfetch;
+
+          # excludeShellChecks = [ "SC2034" "SC2155" "SC2012" "SC2086" "SC1091" "SC2005" "SC2126" ];
+        };
+      };
+    };
+}
