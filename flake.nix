3427b22fe5a90d2a8d2d1b8a6db946f8d06bb7a1
Syner <ploscarusynxy@gmail.com>
2026-04-03 23:18:40 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -11,11 +11,7 @@
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
       perSystem = { pkgs, ... }: {
-        packages.default = pkgs.runCommand "synfetch" { } ''
-          mkdir -p $out/bin
-          cp ${./synfetch} $out/bin/synfetch
-          chmod +x $out/bin/synfetch
-        '';
+      packages.default = pkgs.writeShellScriptBin "synfetch" (builtins.readFile ./synfetch);
       };
     };
 }
